28f13f1ae2dde1c9a61748bf0db97a13e7eba3df
PhilipMa2 <ma_philip2000@hotmail.com>
2023-08-16 14:34:29 -0500

    Improve compatibility

--- compare.py
+++ compare.py
@@ -12,12 +12,58 @@ def preprocess(name):
     file = textract.process(name)
     text = file.decode()
     start = text.find("第一章")
-    if start != -1:
-        text = text[find_2nd(text, "第一章", start):]
-    print(text)
+    second_occurence = text.find("第一章", start + 1)
+    if second_occurence != -1:
+        text = text[second_occurence:]
+    else:
+        text = text[start:]
     texts = re.findall(r'第.{1,5}[章|条][\s\S]*?\s(?=第.+[章|条])', text)
-    texts.append(text[text.rfind('第'):])
+    last_match = re.search(r'条.{1,5}第', text[::-1])
+    texts.append(text[len(text) - last_match.end():])
     return texts
+
+def only_add_new(oldp, newp):
+    run = newp.add_run(new_texts[j])
+    run.font.color.rgb = docx.shared.RGBColor(128,21,0)
+    run = oldp.add_run('新 增')
+    run.font.color.rgb = docx.shared.RGBColor(0,102,51)
+
+def only_add_old(oldp, newp):
+    run = oldp.add_run(old_texts[i])
+    run.font.color.rgb = docx.shared.RGBColor(0,102,51)
+    run = newp.add_run('移 除')
+    run.font.color.rgb = docx.shared.RGBColor(128,21,0)
+
+def add_both(oldp, newp, old_text, new_text):
+    diff = difflib.ndiff(old_text, new_text)
+    # find the difference ratio of two entries
+    seq_mat = difflib.SequenceMatcher()
+    seq_mat.set_seqs(old_text, new_text)
+    ratio = seq_mat.ratio()
+    len_diff = abs(len(old_text) - len(new_text))
+    # if the lengths of two entries are substantially different, then it's 
+    # possible for them to be the same entry even if difference ratio is less
+    # than 0.5. For example, see entry 113 (2021) and 108 (2014)
+    if (ratio < 0.5 and len_diff < 200)  \
+        or (len_diff >= 200 and ratio < 0.1):
+        only_add_new(oldp, newp)
+        return False
+    else:
+        for k, s in enumerate(diff):
+            # same content
+            if s[0] == ' ': 
+                newp.add_run(s[-1])
+                oldp.add_run(s[-1])
+            # if content is added in 2021, make it red in 2021
+            elif s[0] == '+':
+                run = newp.add_run(s[-1])
+                run.font.color.rgb = docx.shared.RGBColor(128,21,0)
+            # if content is deleted in 2021, make it green in 2014
+            elif s[0] == '-':
+                run = oldp.add_run(s[-1])
+                run.font.color.rgb = docx.shared.RGBColor(0,102,51)
+        return True
+
 
 if __name__ == "__main__":
     # specify file name to compare
@@ -38,37 +84,31 @@ if __name__ == "__main__":
         cells = table.add_row().cells
         oldp = cells[0].add_paragraph()
         newp = cells[1].add_paragraph()
-        diff = difflib.ndiff(old_texts[i], new_texts[j])
-        # find the difference ratio of two entries
-        seq_mat = difflib.SequenceMatcher()
-        seq_mat.set_seqs(old_texts[i], new_texts[j])
-        ratio = seq_mat.ratio()
-        len_diff = abs(len(old_texts[i]) - len(new_texts[j]))
-        # if the lengths of two entries are substantially different, then it's 
-        # possible for them to be the same entry even if difference ratio is less
-        # than 0.5. For example, see entry 113 (2021) and 108 (2014)
-        if (ratio < 0.5 and len_diff < 200)  \
-            or (len_diff >= 200 and ratio < 0.1):
-            run = newp.add_run(new_texts[j])
-            run.font.color.rgb = docx.shared.RGBColor(128,21,0)
-            run = oldp.add_run('新 增')
-            run.font.color.rgb = docx.shared.RGBColor(0,102,51)
+        section_pattern = re.compile("^第.{1,5}章[\s\S]*")
+        old_section_end = section_pattern.match(old_texts[i])
+        new_section_end = section_pattern.match(new_texts[j])
+        if old_section_end and not new_section_end:
+            only_add_new(oldp, newp)
             j += 1
-        else:
-            for k, s in enumerate(diff):
-                # same content
-                if s[0] == ' ': 
-                    newp.add_run(s[-1])
-                    oldp.add_run(s[-1])
-                # if content is added in 2021, make it red in 2021
-                elif s[0] == '+':
-                    run = newp.add_run(s[-1])
-                    run.font.color.rgb = docx.shared.RGBColor(128,21,0)
-                # if content is deleted in 2021, make it green in 2014
-                elif s[0] == '-':
-                    run = oldp.add_run(s[-1])
-                    run.font.color.rgb = docx.shared.RGBColor(0,102,51)
+        elif not old_section_end and new_section_end:
+            only_add_old(oldp, newp)
             i += 1
+        else:
+            both_added = add_both(oldp, newp, old_texts[i], new_texts[j])
+            if both_added:
+                i += 1
             j += 1
+    while i < len(old_texts):
+        cells = table.add_row().cells
+        oldp = cells[0].add_paragraph()
+        newp = cells[1].add_paragraph()
+        only_add_old(oldp, newp)
+        i += 1
+    while j < len(new_texts):
+        cells = table.add_row().cells
+        oldp = cells[0].add_paragraph()
+        newp = cells[1].add_paragraph()
+        only_add_new(oldp, newp)
+        j += 1
         
     output.save("output.docx")
