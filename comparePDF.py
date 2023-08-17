c74684f4a0148eacf3242978ecb7f488c866ebbf
PhilipMa2 <ma_philip2000@hotmail.com>
2023-08-17 15:18:51 -0500

    add comparePDF without table function

--- comparePDF.py
+++ comparePDF.py
@@ -92,70 +92,70 @@ if __name__ == "__main__":
     new_text = ''
     old_year = get_year(old_pdf)
     new_year = get_year(new_pdf)
-    old_dfs = []
-    new_dfs = []
+    # old_dfs = []
+    # new_dfs = []
     
-    for i in range(5):
+    for i in range(len(old_pdf.pages)):
         text = old_pdf.pages[i].extract_text()
-        get_table(old_file, i+1, text, old_dfs)
+        # get_table(old_file, i+1, text, old_dfs)
         old_text += text
-    for i in range(5):
+    for i in range(len(new_pdf.pages)):
         text = new_pdf.pages[i].extract_text()
-        get_table(new_file, i+1, text, new_dfs)
+        # get_table(new_file, i+1, text, new_dfs)
         new_text += text
 
-    # old_texts = preprocess(old_text)
-    # new_texts = preprocess(new_text)
+    old_texts = preprocess(old_text)
+    new_texts = preprocess(new_text)
 
     output = docx.Document()
-    # table = output.add_table(rows=1, cols=2, style="Table Grid")
-    # row = table.rows[0]
-    # # get the year of each document
-    # row.cells[0].text = old_year
-    # row.cells[1].text = new_year
+    table = output.add_table(rows=1, cols=2, style="Table Grid")
+    row = table.rows[0]
+    # get the year of each document
+    row.cells[0].text = old_year
+    row.cells[1].text = new_year
 
-    # # i keeps track of old document law and j keeps track of new document law
-    # i = 0
-    # j = 0
-    # while i < len(old_texts) and j < len(new_texts):
-    #     cells = table.add_row().cells
-    #     oldp = cells[0].add_paragraph()
-    #     newp = cells[1].add_paragraph()
-    #     compare_entry(oldp, newp, old_texts[i], new_texts[j])
-    #     i += 1
-    #     j += 1
-    df_all = old_dfs[0].join(new_dfs[0], how='outer', lsuffix='_old', rsuffix='_new')
-    table = output.add_table(df_all.shape[0]+1, df_all.shape[1])
-    for j in range(df_all.shape[-1]):
-        table.cell(0, j).text = df_all.columns[j]
-    i = j = 0
-    wait_i = wait_j = False
-    for k in range(df_all.shape[0]):
-        if i < old_dfs[0].shape[0] and not wait_i:
-            table.cell(k+1, 0).text = str(df_all.values[i, 0])
-            table.cell(k+1, 1).text = str(df_all.values[i, 1])
-            if pd.isna(df_all.values[i, 1]):
-                print('wait')
-                if not wait_j:
-                    wait_i = True
-                else:
-                    wait_j = False
-            i += 1
-        else:
-            table.cell(k+1, 0).text = 'nan'
-            table.cell(k+1, 1).text = 'nan'
-        if j < new_dfs[0].shape[0] and not wait_j:
-            table.cell(k+1, 2).text = str(df_all.values[j, 2])
-            table.cell(k+1, 3).text = str(df_all.values[j, 3])
-            if pd.isna(df_all.values[j, 3]):
-                if not wait_i:
-                    wait_j = True
-                else:
-                    wait_i = False
-            j += 1
-        else:
-            table.cell(k+1, 2).text = 'nan'
-            table.cell(k+1, 3).text = 'nan'
+    # i keeps track of old document law and j keeps track of new document law
+    i = 0
+    j = 0
+    while i < len(old_texts) and j < len(new_texts):
+        cells = table.add_row().cells
+        oldp = cells[0].add_paragraph()
+        newp = cells[1].add_paragraph()
+        compare_entry(oldp, newp, old_texts[i], new_texts[j])
+        i += 1
+        j += 1
+    # df_all = old_dfs[0].join(new_dfs[0], how='outer', lsuffix='_old', rsuffix='_new')
+    # table = output.add_table(df_all.shape[0]+1, df_all.shape[1])
+    # for j in range(df_all.shape[-1]):
+    #     table.cell(0, j).text = df_all.columns[j]
+    # i = j = 0
+    # wait_i = wait_j = False
+    # for k in range(df_all.shape[0]):
+    #     if i < old_dfs[0].shape[0] and not wait_i:
+    #         table.cell(k+1, 0).text = str(df_all.values[i, 0])
+    #         table.cell(k+1, 1).text = str(df_all.values[i, 1])
+    #         if pd.isna(df_all.values[i, 1]):
+    #             print('wait')
+    #             if not wait_j:
+    #                 wait_i = True
+    #             else:
+    #                 wait_j = False
+    #         i += 1
+    #     else:
+    #         table.cell(k+1, 0).text = 'nan'
+    #         table.cell(k+1, 1).text = 'nan'
+    #     if j < new_dfs[0].shape[0] and not wait_j:
+    #         table.cell(k+1, 2).text = str(df_all.values[j, 2])
+    #         table.cell(k+1, 3).text = str(df_all.values[j, 3])
+    #         if pd.isna(df_all.values[j, 3]):
+    #             if not wait_i:
+    #                 wait_j = True
+    #             else:
+    #                 wait_i = False
+    #         j += 1
+    #     else:
+    #         table.cell(k+1, 2).text = 'nan'
+    #         table.cell(k+1, 3).text = 'nan'
         
     output.save("output.docx")
 
